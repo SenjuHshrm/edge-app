9369e6703803fab3e58640fdbeb87dbb29709eb8
CreativeBrainStudio <102801479+CreativeBrainStudio@users.noreply.github.com>
2022-06-16 19:33:04 +0800

    tempalte
    carlo

--- src/app/key-partners/home/my-inventory/my-inventory.component.ts
+++ src/app/key-partners/home/my-inventory/my-inventory.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-my-inventory',
   templateUrl: './my-inventory.component.html',
-  styleUrls: ['./my-inventory.component.scss']
+  styleUrls: ['./my-inventory.component.scss'],
 })
 export class MyInventoryComponent implements OnInit {
+  public tableData = [{}, {}, {}, {}, {}];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
