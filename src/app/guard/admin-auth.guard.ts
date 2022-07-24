eec1f1054453287ccb34ecfe25f5742f078000b2
EnzGyb <rdimasapit.bos@gmail.com>
2022-07-24 22:06:48 +0800

    guards, todo code for intercepting 401 response, token handler service

--- src/app/guard/admin-auth.guard.ts
+++ src/app/guard/admin-auth.guard.ts
@@ -5,15 +5,15 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
-
+export class AdminAuthGuard implements CanActivate {
+  
   canActivate(route: ActivatedRouteSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    let token: string | null = localStorage.getItem('ACCESS')
-    if(!token) {
-      window.location.href = '/login'
-      return false;
-    }
-    return true;
+      // let token: string | null = localStorage.getItem('ACCESS')
+      // if(!token) {
+      //   window.location.href = 'admin/login'
+      //   return false;
+      // }
+      return true;
   }
   
 }
