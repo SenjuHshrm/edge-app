78aed037f8fe7a1f42a0ffc44f084ce55341efc5
EnzGyb <rdimasapit.bos@gmail.com>
2022-08-24 17:27:47 +0800

    landing page

--- src/app/components/bar-chart/bar-chart.component.ts
+++ src/app/components/bar-chart/bar-chart.component.ts
@@ -1,5 +1,21 @@
+import { BookingService } from './../../services/booking.service';
 import { Component, OnInit } from '@angular/core';
-import { ApexAxisChartSeries, ApexChart } from 'ng-apexcharts';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle
+} from 'ng-apexcharts';
+import * as moment from 'moment';
+import jwtDecode from 'jwt-decode';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+};
 
 @Component({
   selector: 'app-bar-chart',
@@ -7,19 +23,55 @@ import { ApexAxisChartSeries, ApexChart } from 'ng-apexcharts';
   styleUrls: ['./bar-chart.component.scss'],
 })
 export class BarChartComponent implements OnInit {
-  series: ApexAxisChartSeries = [
-    {
-      name: 'My-series',
-      data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
-    },
-  ];
-  chart: ApexChart = {
-    height: 500,
-    width: '100%',
-    type: 'bar',
+
+  // series: ApexAxisChartSeries = [
+  //   {
+  //     name: 'My-series',
+  //     data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+  //   },
+  // ];
+  // chart: ApexChart = {
+  //   height: 500,
+  //   width: '100%',
+  //   type: 'bar',
+  // };
+
+  public chartOptions: ChartOptions = {
+    series: [{  name: '', data: []}],
+    chart: { height: 0, width: '', type: 'bar' },
+    title: { text: '' },
+    xaxis: { categories: [] }
   };
 
-  constructor() {}
+  constructor(
+    private booking: BookingService
+  ) {
+    let days = []
+    for(let i = 0; i < moment().daysInMonth(); i++) {
+      days.push((i+1).toString())
+    }
+    this.chartOptions = {
+      series: [
+        {
+          name: 'Booking',
+          data: []
+        }
+      ],
+      chart: {
+        height: 500,
+        width: '100%',
+        type: 'bar',
+      },
+      title: {
+        text: 'Booking'
+      },
+      xaxis: {
+        categories: days
+      }
+    }
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    
+  }
 }
